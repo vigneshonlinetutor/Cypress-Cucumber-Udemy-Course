55079ad6ca36fe80381e251f00e035eca02ba56b
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-05-28 22:37:02 +0200

    Added TestData using Fixture and DataTable

--- cypress/e2e/cucumber/stepDefinitions/Tc01.feature
+++ cypress/e2e/cucumber/stepDefinitions/Tc01.feature
@@ -5,6 +5,8 @@ Feature: Conduit Login Functionality
 Scenario: Login and Logout with valid credentials
 Given I am on the Conuduit Login page
 When I login with valid credentials
+|username             | password    |
+|cypressdemo@gmail.com| cypressdemo |
 And I click on the settings button
 And I click on the Logout Button
 Then I route back to the Login page
--- cypress/e2e/cucumber/stepDefinitions/Tc01/Tc01_StepDef.js
+++ cypress/e2e/cucumber/stepDefinitions/Tc01/Tc01_StepDef.js
@@ -2,14 +2,24 @@
 
 import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
 
+beforeEach(function(){
+    cy.fixture('ConduitData').as('data')
+})
+
 Given('I am on the Conuduit Login page', function () {
     cy.visit('https://react-redux.realworld.io/')
     cy.get('a[href="#login"]').click()
 })
 
-When('I login with valid credentials', function () {
-    cy.get('input[placeholder="Email"]').type('cypressdemo@gmail.com')
-    cy.get('input[placeholder="Password"]').type('cypressdemo')
+When('I login with valid credentials', function (dataTable) {
+    /*
+    When using fixture
+    cy.get('input[placeholder="Email"]').type(this.data.email)
+    cy.get('input[placeholder="Password"]').type(this.data.password)
+    */
+    
+    cy.get('input[placeholder="Email"]').type(dataTable.rawTable[1][0])
+    cy.get('input[placeholder="Password"]').type(dataTable.rawTable[1][1])
     cy.get('button[type="submit"]').click()
 })
 
